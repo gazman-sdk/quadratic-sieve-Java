50c619c88bf12453cee749825a939dcfcc6e7904
Ilya Gazman <gazman1986@gmail.com>
2020-07-22 14:25:43 -0400

    Episode 1: Fermat's factorization method

---
+++ src/com/gazman/quadratic_sieve/BaseFact.java
@@ -0,0 +1,36 @@
+package com.gazman.quadratic_sieve;
+
+import java.math.BigInteger;
+import java.util.Random;
+
+public abstract class BaseFact {
+    private static final long startTime = System.nanoTime();
+
+    protected void start(int bitLLength) {
+        Random random = new Random(123);
+
+        BigInteger A = new BigInteger(bitLLength - 1, random).nextProbablePrime();
+        BigInteger B = new BigInteger(bitLLength + 1, random).nextProbablePrime();
+        BigInteger N = A.multiply(B);
+
+        log("A", A);
+        log("B", B);
+        log("N", N);
+
+        solve(N);
+    }
+
+    protected abstract void solve(BigInteger n);
+
+    protected void log(Object... objects) {
+        StringBuilder out = new StringBuilder();
+        for (Object object : objects) {
+            out.append(object).append(" ");
+        }
+
+        long milliseconds = (System.nanoTime() - startTime) / 1_000_000;
+
+        String prefix = milliseconds + "> ";
+        System.out.println(prefix + out);
+    }
+}
---
+++ src/com/gazman/quadratic_sieve/QuadraticSieve.java
@@ -0,0 +1,33 @@
+package com.gazman.quadratic_sieve;
+
+import java.math.BigInteger;
+import java.util.Random;
+
+public class QuadraticSieve extends BaseFact {
+
+    public static void main(String[] args) {
+        new QuadraticSieve().start(22);
+    }
+
+    @Override
+    protected void solve(BigInteger N) {
+        Random random = new Random(123);
+
+        for (int i = 0; i < 10; ) {
+            BigInteger b = new BigInteger(N.bitLength() / 2 + 1, random).abs();
+            BigInteger a = b.pow(2).subtract(N);
+            if(isSquare(a)){
+                log("found it", b.subtract(a.sqrt()));
+                i++;
+            }
+
+        }
+    }
+
+    private boolean isSquare(BigInteger a) {
+        if(a.compareTo(BigInteger.ZERO) < 0){
+            return false;
+        }
+        return a.sqrt().pow(2).equals(a);
+    }
+}
