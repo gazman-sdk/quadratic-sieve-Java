460302495d859d1a167d5612042310b3acb91004
Ilya Gazman <gazman1986@gmail.com>
2020-07-24 09:15:20 -0400

    episode 2.1

--- src/com/gazman/quadratic_sieve/Episode2.java
+++ src/com/gazman/quadratic_sieve/Episode2.java
@@ -1,31 +1,25 @@
 package com.gazman.quadratic_sieve;
 
 import java.math.BigInteger;
-import java.util.Random;
 
 public class Episode2 extends BaseFact {
 
     public static void main(String[] args) {
-        new Episode2().start(70);
+        new Episode2().start(60);
     }
 
     @Override
     protected void solve(BigInteger N) {
 
-        BigInteger sqrt = N.sqrt();
-        for (int i = 1, k = 1; ; i++) {
-            if(i % 100 == 0){
-                k++;
-                sqrt = N.multiply(BigInteger.valueOf(k)).sqrt();
-                i = 0;
-            }
-            BigInteger v = BigInteger.valueOf(i);
-            BigInteger a = sqrt.add(v);
+
+        for (int i = 0; ; i++) {
+            BigInteger a = N.sqrt().add(BigInteger.valueOf(i));
             BigInteger mod = a.pow(2).mod(N);
-            if(isSquare(mod)){
-                BigInteger maybeASolution = a.add(mod.sqrt()).gcd(N);
-                if(!maybeASolution.equals(N) && !maybeASolution.equals(BigInteger.ONE)){
-                    log(k, "Oh yeah", maybeASolution);
+
+            if (isSquare(mod)) {
+                BigInteger maybeSolution = a.add(mod.sqrt()).gcd(N);
+                if (!maybeSolution.equals(N) && !maybeSolution.equals(BigInteger.ONE)) {
+                    log("Oh yeah", maybeSolution);
                     break;
                 }
             }
@@ -33,7 +27,7 @@ public class Episode2 extends BaseFact {
     }
 
     private boolean isSquare(BigInteger a) {
-        if(a.compareTo(BigInteger.ZERO) < 0){
+        if (a.compareTo(BigInteger.ZERO) < 0) {
             return false;
         }
         return a.sqrt().pow(2).equals(a);
